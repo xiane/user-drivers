18b29c406db4daeaec146f018e2516a30370ae51
Luke go <sangch.go@gmail.com>
2023-04-18 17:21:15 +0900

    WeatherBoard: Add weather board driver.
    Signed-off-by: Luke go <sangch.go@gmail.com>

---
+++ WeatherBoard/src/main/java/com/hardkernel/odroid/things/contrib/WeatherBoard/WeatherBoard.java
@@ -0,0 +1,106 @@
+package com.hardkernel.odroid.things.contrib.WeatherBoard;
+
+import com.google.android.things.pio.PeripheralManager;
+
+import java.util.List;
+
+public class WeatherBoard {
+    /**
+     *  Android Things Peripheral Manager
+     */
+    private PeripheralManager manager;
+
+    /**
+     *  UV, Visible, IR
+     */
+    private SI1132 si1132;
+    /**
+     *  Temperature, Humidity, Pressure, Altitude
+     */
+    private BME280 bme280;
+
+    public WeatherBoard(String i2c) {
+        // get Peripheral Manager for managing the i2c.
+        manager = PeripheralManager.getInstance();
+
+        /*
+          get available i2c pin list.
+          i2c name format - i2c-#, and n2/c4 have I2C-1 and I2C-2.
+          If given i2c name is in list, use it.
+         */
+        List<String> i2cList = manager.getI2cBusList();
+
+        try {
+            String i2cBusName;
+
+            if (i2cList.contains(i2c))
+                i2cBusName = i2c;
+            else
+                i2cBusName = i2cList.get(0);
+
+            si1132 = new SI1132(manager.openI2cDevice(i2cBusName, SI1132.ADDRESS));
+            bme280 = new BME280(manager.openI2cDevice(i2cBusName, BME280.ADDRESS));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public WeatherBoard() {
+        // get Peripheral Manager for managing the i2c.
+        manager = PeripheralManager.getInstance();
+
+        /*
+          get available i2c pin list.
+          i2c name format - I2C-#, and n2/c4 have I2C-1 and I2C-2.
+          In this case, i2c-2 is used.
+         */
+        List<String> i2cList = manager.getI2cBusList();
+        String i2c = i2cList.get(0);
+
+        try {
+            si1132 = new SI1132(manager.openI2cDevice(i2c, SI1132.ADDRESS));
+            bme280 = new BME280(manager.openI2cDevice(i2c, BME280.ADDRESS));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean init() {
+        try {
+            si1132.init();
+            byte[] sampling = {BME280.POWER_MODE.NORMAL, 0x02, 0x02, 0x02};
+            bme280.init(sampling);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public short readUV() throws Exception {
+        return si1132.readUV();
+    }
+
+    public double readVisible() throws Exception {
+        return si1132.readVisible();
+    }
+
+    public double readIR() throws Exception {
+        return si1132.readIR();
+    }
+
+    public double readHumidity() throws Exception {
+        return bme280.readHumidity();
+    }
+
+    public double readTemperature() throws Exception {
+        return bme280.readTemperature();
+    }
+
+    public double readPressure() throws Exception {
+        return bme280.readPressure();
+    }
+
+    public double readAltitude(double pressure ,double seaLevel) {
+        return 44330.0 * (1.0 - Math.pow(pressure/seaLevel, 0.1903));
+    }
+}
---
+++ WeatherBoard/src/test/java/com/hardkernel/odroid/things/contrib/WeatherBoard/WeatherBoardTest.java
@@ -0,0 +1,14 @@
+package com.hardkernel.odroid.things.contrib.WeatherBoard;
+
+import com.google.android.things.pio.I2cDevice;
+
+import org.junit.Test;
+
+import org.mockito.Mock;
+
+public class WeatherBoardTest {
+    @Mock
+    I2cDevice i2c;
+
+    @Test
+}
